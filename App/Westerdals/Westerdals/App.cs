6620ecffe4ea1686fc546d1c61f33f233dbf78ea
Alexander144 <Alexan2405@gmail.com>
2015-11-18 11:43:50 +0100

    Fikset ting

--- App/Westerdals/Westerdals/App.cs
+++ App/Westerdals/Westerdals/App.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using Westerdals.Pages;
 using Xamarin.Forms;
 
 namespace Westerdals
@@ -12,19 +12,7 @@ namespace Westerdals
 		public App()
 		{
 			// The root page of your application
-			MainPage = new ContentPage
-			{
-				Content = new StackLayout
-				{
-					VerticalOptions = LayoutOptions.Center,
-					Children = {
-						new Label {
-							XAlign = TextAlignment.Center,
-							Text = "Welcome to Xamarin Forms!"
-						}
-					}
-				}
-			};
+			MainPage = new MainPage();
 		}
 
 		protected override void OnStart()
