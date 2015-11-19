40345a7496dbc4978ba7b18065f6d58f3a1e0961
Daniel Alexander Hagen <Daniel@Daniels-MacBook-Pro-2.local>
2015-11-19 14:43:44 +0100

    lagt til css kode

--- php/register.php
+++ php/register.php
@@ -7,6 +7,8 @@ include_once 'includes/functions.php';
     <head>
         <meta charset="UTF-8">
         <title>Secure Login: Registration Form</title>
+        <link rel="stylesheet" type="text/css" href="css/main.css">
+        <link href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300' rel='stylesheet' type='text/css'>
         <script type="text/JavaScript" src="js/sha512.js"></script> 
         <script type="text/JavaScript" src="js/forms.js"></script>
         <link rel="stylesheet" href="styles/main.css" />
@@ -14,6 +16,7 @@ include_once 'includes/functions.php';
     <body>
         <!-- Registration form to be output if the POST variables are not
         set or if the registration script caused an error. -->
+        <div id="registrationPage">
         <h1>Register with us</h1>
         <?php
         if (!empty($error_msg)) {
@@ -36,24 +39,27 @@ include_once 'includes/functions.php';
         <form action="<?php echo esc_url($_SERVER['PHP_SELF']); ?>" 
                 method="post" 
                 name="registration_form">
-            Username: <input type='text' 
+            <div id="registration">
+            Username: <input class="registerField" type='text' 
                 name='username' 
                 id='username' /><br>
-            Email: <input type="text" name="email" id="email" /><br>
-            Password: <input type="password"
+            Email: <input class="registerField" type="text" name="email" id="email" /><br>
+            Password: <input class="registerField" type="password"
                              name="password" 
                              id="password"/><br>
-            Confirm password: <input type="password" 
+            Confirm password: <input class="RegisterField" type="password" 
                                      name="confirmpwd" 
                                      id="confirmpwd" /><br>
-            <input type="button" 
+            <input id="registrationBTN" type="button" 
                    value="Register" 
                    onclick="return regformhash(this.form,
                                    this.form.username,
                                    this.form.email,
                                    this.form.password,
-                                   this.form.confirmpwd);" /> 
+                                   this.form.confirmpwd);" />
+            </div>
         </form>
         <p>Return to the <a href="index.php">login page</a>.</p>
+        </div>
     </body>
 </html>
