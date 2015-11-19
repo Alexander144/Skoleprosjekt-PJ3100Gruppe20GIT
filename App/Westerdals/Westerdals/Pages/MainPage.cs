676c8dc186efab84ff3939fd795872c2882e4009
Alexander144 <Alexan2405@gmail.com>
2015-11-19 10:19:34 +0100

    Fler sider og knapper

--- App/Westerdals/Westerdals/Pages/MainPage.cs
+++ App/Westerdals/Westerdals/Pages/MainPage.cs
@@ -9,6 +9,10 @@ namespace Westerdals.Pages
 {
 	public class MainPage : ContentPage
 	{
+		public Button btn2;
+		public ListView list;
+		private int click = 0;
+		
 		public MainPage()
 		{
 			int a = 0;
@@ -16,18 +20,47 @@ namespace Westerdals.Pages
 			{
 				Text = DateTime.Now.ToString("yyyy-MM hh:mm:ss.fff")
 			};
+		
 			var btn = new Button
 			{
 				Text = "Click here!"
 			};
-			btn.Clicked += (s, e) => lbl.Text = DateTime.Now.ToString("yyyy-MM hh:mm:ss.fff");
+
+			list = new ListView
+			{
+				RowHeight = 20
+		};
+
+			btn2 = new Button
+			{
+				Text = "Kill Alex",
+                BackgroundColor = Color.Red,
+				FontSize = 30,
+				WidthRequest = 150
+			};
+
+			btn2.Clicked += OnButtonClicked;
+
+			
+
 			var layout = new StackLayout
 			{
 				Spacing = 10,
-				Children = {btn, lbl }
-			};
+				Children = {btn, lbl, btn2, list},
+		};
 			this.Content = layout;
 			this.Padding = 15;
 		}
+
+		void OnButtonClicked(object sender, EventArgs e)
+		{
+			
+
+				list.ItemsSource = new string[]
+				{
+					"With a chair",
+				};
+		}
+
 	}
 }
