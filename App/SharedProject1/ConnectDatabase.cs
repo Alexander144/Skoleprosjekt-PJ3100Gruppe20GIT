ac88c30e95bc95e905a3a5f88fe95e295361ac4a
Alexander144 <Alexan2405@gmail.com>
2015-11-20 11:03:50 +0100

    Nå starter Appen

---
+++ App/SharedProject1/ConnectDatabase.cs
@@ -0,0 +1,19 @@
+﻿using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedProject1
+{
+    public class ConnectDatabase
+    {
+		private Button button;
+
+
+		public ConnectDatabase(Button button)
+		{
+			int count = 0;
+			button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+		}
+    }
+}
