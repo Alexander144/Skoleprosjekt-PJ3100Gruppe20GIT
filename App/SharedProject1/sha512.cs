68a8037df63c4afc394d26f363b9eb7bacb9609a
Alexander144 <Alexan2405@gmail.com>
2015-11-21 16:35:21 +0100

    sha512

---
+++ App/SharedProject1/sha512.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SharedProject1
+{
+   public class sha512
+    {
+
+		private string hashData;
+		public sha512(string password)
+		{
+			UnicodeEncoding UE = new UnicodeEncoding();
+			byte[] message = UE.GetBytes(password);
+			SHA512Managed hashString = new SHA512Managed();
+			string hexNumber = "";
+			byte[] hashValue = hashString.ComputeHash(message);
+			foreach (byte x in hashValue)
+			{
+				hexNumber += String.Format("{0:x2}", x);
+			}
+			hashData = hexNumber;
+		}
+		public string GetHashData()
+		{
+			return hashData;
+
+		}
+	}
+}
