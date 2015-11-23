9128fb91a93884dea1c3b457b41e64036840cba2
Marthe Nikoline Elsrud <elsmar14@student.westerdals.no>
2015-11-23 13:54:29 +0100

    Kodet html i index.php og css

---
+++ php/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Westerdals</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+
+
+	<div id="container">
+
+		<header id="mainPageHeader" class="col">
+			<img src="../images/WACT_hovedlogo_sort_rgb.png" class="col headerImg">
+		</header>
+
+		<div class="main-nav">
+			<nav class="col" id="mainPageNavigation">
+				<ul>
+					<li><a href>Jobber</a></li>
+					<li><a href>Legg til stillinger</a></li>
+				</ul>
+			</nav>
+		</div>
+
+
+		<aside id="bannerFront">
+			<img src="" alt="Banner Frontpage" class="col img-bannerFront">
+		</aside>
+
+		<section id="mainContent">
+
+
+			<article class="col col-6">
+				<a href=""><h3>Prosjekt 1</h3></a>
+				<p>Text</p>
+			</article>
+
+			<article class="col col-6">
+				<a href=""><h3>Prosjekt 2</h3></a>
+				<p>Text</p>
+			</article>
+
+		</section>
+	</div>
+
+	<footer id="mainPageFooterWrap">
+		<img src="../images/WACT_hovedlogo_farge_rgb.png" class="col footerImg">
+	</footer>
+
+</body>
+</html>
+
