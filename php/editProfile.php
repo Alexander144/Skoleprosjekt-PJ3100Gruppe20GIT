67a9045e32feda3d2739ef2e6e53f3bd4828269e
Marthe Nikoline Elsrud <elsmar14@student.westerdals.no>
2016-02-29 11:37:33 +0100

    La til PHP variabler

--- php/editProfile.php
+++ php/editProfile.php
@@ -37,7 +37,7 @@
 
     <div id="profileBasicInfo" class="col col-3">
         <h1>Studentens navn</h1>
-        <!--<img id="profilePic" src="img/profilbildeStudent.jpg" alt="Profilbilde av studenten (Mr. Bean)">-->
+        <img id="profilePic" src="img/profilbildeStudent.jpg" alt="Profilbilde av studenten (Mr. Bean)" <?php echo $profileImgEdit?>>
         <form action="includes/upload.php" method="post" enctype="multipart/form-data">
     <p>Last opp nytt profilbilde</p>
     <input type="file" name="fileToUpload" id="fileToUpload">
@@ -57,42 +57,15 @@
         
         <div id ="profileText" class="col col-3">
             <h3 id="AboutMe">Info om meg:</h3>
-            <textarea id="editAboutMe">Med 17 års fartstid i bransjen har Are vært borti de fleste problemstillinger rundt web og interaktive medier. Han har hatt en finger med i en rekke av landets mest profilerte nettsteder og intranett.Are Gjertin Urkegjerde Halland. Han har vært en sentral skikkelse i fagmiljøet, og grunnla blant annet det faglige nettverket UXnet. Han er også hjernen bak Netlife Research sin egen kjernemodell. Are brenner for å skape gode brukeropplevelser som bygger bro mellom brukerbehov, strategi, innhold og design. Han har spisskompetanse på blant annet konsept, strategi, navigasjon, søk og innhold.Faglig har han bakgrunn fra medievitenskap hovedfag ved Universitetet i Oslo og informasjonsutdanninga ved Høgskulen i Volda, og har jobbet i Netlife Research siden 2006. Før dette jobbet han som nettjournalist i Norges første internett-portal Origo fra 1995-97 og fra 1998-2006 som informasjonsarkitekt i Neo Interaktiv, Icon Medialab og WM-data UX.</textarea>
+            <textarea id="editAboutMe" <?php echo $profileEditAboutMe?>>Med 17 års fartstid i bransjen har Are vært borti de fleste problemstillinger rundt web og interaktive medier. Han har hatt en finger med i en rekke av landets mest profilerte nettsteder og intranett.Are Gjertin Urkegjerde Halland. Han har vært en sentral skikkelse i fagmiljøet, og grunnla blant annet det faglige nettverket UXnet. Han er også hjernen bak Netlife Research sin egen kjernemodell. Are brenner for å skape gode brukeropplevelser som bygger bro mellom brukerbehov, strategi, innhold og design. Han har spisskompetanse på blant annet konsept, strategi, navigasjon, søk og innhold.Faglig har han bakgrunn fra medievitenskap hovedfag ved Universitetet i Oslo og informasjonsutdanninga ved Høgskulen i Volda, og har jobbet i Netlife Research siden 2006. Før dette jobbet han som nettjournalist i Norges første internett-portal Origo fra 1995-97 og fra 1998-2006 som informasjonsarkitekt i Neo Interaktiv, Icon Medialab og WM-data UX.</textarea>
         </div>
 
-        <div id="ProfileProjects" class="col col-3">
-            <h1>Mine Projekter:</h1>
-            <article class="projectBoxes">
-                <h3>Projekt 1</h3>
-                <p></p>
-            </article>
-            <article class="projectBoxes">
-                <h3>Projekt 2</h3>
-                <p></p>
-            </article>
-            <article class="projectBoxes">
-                <h3>Projekt 3</h3>
-                <p></p>
-            </article>
-            <br>
+        <div id="ProfileProjects" class="col col-3" <?php echo $profileProject?>>
+
             <input id="ProfileBTN" class="buttonDesign" type="button" onclick="alert('Legg til Prosjekt')" value="Legg til prosjekt">
         </div>
       
         <div id="ProfilePopular" class="col col-3">
-            
-                        <h1>Mine Projekter:</h1>
-            <article class="projectBoxes">
-                <h3>Projekt 1</h3>
-                <p></p>
-            </article>
-            <article class="projectBoxes">
-                <h3>Projekt 2</h3>
-                <p></p>
-            </article>
-            <article class="projectBoxes">
-                <h3>Projekt 3</h3>
-                <p></p>
-            </article>
       
       </div>
 
