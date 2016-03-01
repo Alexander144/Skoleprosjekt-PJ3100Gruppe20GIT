47bd2e34be1a499d4429136eb05b81351c2ae289
nuranes <uranes743@hotmail.com>
2016-03-01 11:01:11 +0100

    allProjects med mal

--- php/allProjects.php
+++ php/allProjects.php
@@ -1,83 +1,86 @@
-<?php include_once 'header.php'; ?>
-<!-- Entry of body content field for index below -->
+<?php include_once 'header.php';
+      /*include_once 'includes/editProfile.inc.php';*/
+ ?>
+        <?php if (login_check($mysqli) == true) : ?>
+		 <form action="<?php echo esc_url($_SERVER['PHP_SELF']); ?>" 
+                method="post" 
+                name="updateProfile_form">
+             
+             <!--start page-->
+             
+             <section id="mainContent"> <!-- start Main Content -->
+                 <div class="sort-container col"> <!-- start sorting content-->
+                    <ul>
 
-  <section id="mainContent"> <!-- start Main Content -->
+                        <li class="sort-workBy sortMenu"><a href="#" <?php $sortProject; ?>>Arbeid av</a>
+                      <ul>
+                        <li><a href="#" <?php $sortProjectStudent; ?>>Student</a></li>
+                        <li><a href="#" <?php $sortProjectAlumni; ?>>Alumni</a></li>
+                      </ul>
+                    </li>
 
-  <div class="sort-container col"> <!-- start sorting content-->
-        <ul>
-        
-            <li class="sort-workBy sortMenu"><a href="#" <?php $sortProject; ?>>Arbeid av</a>
-          <ul>
-            <li><a href="#" <?php $sortProjectStudent; ?>>Student</a></li>
-            <li><a href="#" <?php $sortProjectAlumni; ?>>Alumni</a></li>
-          </ul>
-        </li>
+                    <li class="sort-program sortMenu" <?php $sortProgram; ?>><a href="#">Avdeling</a>
+                      <ul>
+                        <li><a id="teknologi" href="#" <?php $sortProgramTeknologi; ?>>Teknologi/IT</a></li>
+                        <li><a id="ledelse" href="#" <?php $sortProgramLedelse; ?>>Ledelse</a></li>
+                        <li><a id="kommunikasjon" href="#" <?php $sortProgramKommunikasjon; ?>>Kommunikasjon</a></li>
+                        <li><a id="kunstfag" href="#" <?php $sortProgramKunstfag; ?>>Kunstfag</a></li>
+                        <li><a id="filmTVSpill" href="#" <?php $sortProgramFilmTVSpill; ?>>Film, TV og Spill</a></li>
+                      </ul>
+                    </li>
 
-        <li class="sort-program sortMenu" <?php $sortProgram; ?>><a href="#">Avdeling</a>
-          <ul>
-            <li><a id="teknologi" href="#" <?php $sortProgramTeknologi; ?>>Teknologi/IT</a></li>
-            <li><a id="ledelse" href="#" <?php $sortProgramLedelse; ?>>Ledelse</a></li>
-            <li><a id="kommunikasjon" href="#" <?php $sortProgramKommunikasjon; ?>>Kommunikasjon</a></li>
-            <li><a id="kunstfag" href="#" <?php $sortProgramKunstfag; ?>>Kunstfag</a></li>
-            <li><a id="filmTVSpill" href="#" <?php $sortProgramFilmTVSpill; ?>>Film, TV og Spill</a></li>
-          </ul>
-        </li>
+                    <li class="sort-orderBy sortMenu" <?php $sortBy; ?>><a href="#">Rekkefølge</a>
+                      <ul>
+                        <li><a href="#" <?php $sortByMostPopular; ?>>Mest Populære</a></li>
+                        <li><a href="#" <?php $sortByNewest; ?>>Nyeste</a></li>
+                      </ul>
+                    </li>
+                  </ul>
 
-        <li class="sort-orderBy sortMenu" <?php $sortBy; ?>><a href="#">Rekkefølge</a>
-          <ul>
-            <li><a href="#" <?php $sortByMostPopular; ?>>Mest Populære</a></li>
-            <li><a href="#" <?php $sortByNewest; ?>>Nyeste</a></li>
-          </ul>
-        </li>
-      </ul>
+                 </div> <!-- end sorting content -->
 
-  </div> <!-- end sorting content -->
+                 <div id="projects"><!-- start projects -->
+                     <?php $projectBox1; ?>
+                 </div><!--end projects-->
 
-  <!-- start projects -->
+             </section> <!-- end Main Content -->
 
-    <article class="col col-3 projectBoxes" <?php echo $projectBox1; ?>>
-      <h3>Prosjekt 1</h3>
-      <p></p>
-    </article>
+             <script src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
+             
+             <script>
+             //Get all projects:
+                 var allProjects = 6; //Get value here
 
-    <article class="col col-3 projectBoxes" <?php echo $projectBox2; ?>>
-      <h3>Prosjekt 2</h3>
-      <p></p>
-    </article>
+                 for(var i = 0; i < allProjects; i++){
+                     var $newProject = $("<div>")
+                        .addClass("col col-3 projectBoxes")
+                     
+                     $("#projects").append($newProject);
+                 }
 
-    <article class="col col-3 projectBoxes" <?php echo $projectBox3; ?>>
-      <h3>Prosjekt 3</h3>
-      <p></p>
-    </article>
+             </script><!--end script-->
+             <!-- End of body content field -->
 
-    <article class="col col-3 projectBoxes" <?php echo $projectBox4; ?>>
-      <h3>Prosjekt 4</h3>
-      <p></p>
-    </article>
-      
-    <article class="col col-3 projectBoxes" <?php echo $projectBox5; ?>>
-      <h3>Prosjekt 5</h3>
-      <p></p>
-    </article>
-      
-    <article class="col col-3 projectBoxes" <?php echo $projectBox6; ?>>
-      <h3>Prosjekt 6</h3>
-      <p></p>
-    </article>
-      
-    <article class="col col-3 projectBoxes" <?php echo $projectBox7; ?>>
-      <h3>Prosjekt 7</h3>
-      <p></p>
-    </article>
-      
-    <article class="col col-3 projectBoxes" <?php echo $projectBox8; ?>>
-      <h3>Prosjekt 8</h3>
-      <p></p>
-    </article>
+             <?php include_once 'footer.php'; ?>
+             
+             <!--end page-->
+             
 
-  <!-- end projects -->
-
-  </section> <!-- end Main Content -->
-	</div> <!-- end container -->
-<!-- End of body content field -->
-<?php include_once 'footer.php'; ?>
+             <!-- Knapp funkjsonen-->
+             <input id="UpdateBTN" type="button" 
+                   value="Update" 
+                   onclick="return ProfileUpdateForms(
+                                    this.form,
+                                   this.form.picture,
+                                   this.form.profileEditAboutMe,
+                                   this.form.grades,
+                                   this.form.cv);" />
+        </form>
+            <p>Return to <a href="index.php">login page</a></p>
+        <?php else : ?>
+            <p>
+                <span class="error">You are not authorized to access this page.</span> Please <a href="index.php">login</a>.
+            </p>
+        <?php endif; ?>
+    </body>
+</html>
