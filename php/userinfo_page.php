db8a3dc86ef17fab85f0e117c51ed7c0a8a79c6e
nuranes <uranes743@hotmail.com>
2016-03-02 10:51:58 +0100

    project loop

--- php/userinfo_page.php
+++ php/userinfo_page.php
@@ -45,17 +45,28 @@
                 
         <div id="" class="col col-ProfileStudent">
             <h3>Mine Projekter:</h3>
-            <article class="projectBoxes">
-                <h3>Projekt 1</h3>
-                <p></p>
-            </article>
+            <div id="projects">
+            <?php $projectBox1; ?>
+
+            </div><!--end projects-->
 
             <input id="ProfileBTN" class="buttonDesign" type="button" onclick="alert('Legg til Prosjekt')" value="Legg til et prosjekt">
         </div>
 
-            </div>
-
         </form>
+
+    <script src="http://code.jquery.com/jquery-2.1.4.min.js"></script><script>
+    //Get all projects:
+        var allProjects = 2; //Get value here
+
+        for(var i = 0; i < allProjects; i++){
+            var $newProject = $("<div>")
+                .addClass("col col-3 projectBoxes")
+
+            $("#projects").append($newProject);
+        }
+
+    </script><!--end script-->
 
             <!--<p id="returnLogin" class="col col-3">Return to <a href="index.php">login page</a></p>-->
         <?php else : ?>
