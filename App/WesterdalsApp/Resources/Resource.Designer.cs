848712d7c633be880c825661dcf4f2e28f3a40a9
Kevin Aslan <kevin_aslan@hotmail.com>
2016-03-03 20:41:26 +0100

    52

--- App/WesterdalsApp/Resources/Resource.Designer.cs
+++ App/WesterdalsApp/Resources/Resource.Designer.cs
@@ -60,38 +60,41 @@ namespace WesterdalsApp
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050006
-			public const int Email = 2131034118;
-			
-			// aapt resource value: 0x7f05000a
-			public const int Login = 2131034122;
-			
-			// aapt resource value: 0x7f050007
-			public const int Password = 2131034119;
-			
 			// aapt resource value: 0x7f050001
-			public const int button1 = 2131034113;
-			
-			// aapt resource value: 0x7f050003
-			public const int button2 = 2131034115;
+			public const int Email = 2131034113;
 			
 			// aapt resource value: 0x7f050005
-			public const int button3 = 2131034117;
+			public const int Login = 2131034117;
 			
-			// aapt resource value: 0x7f050004
-			public const int button4 = 2131034116;
+			// aapt resource value: 0x7f050007
+			public const int MessageButton = 2131034119;
 			
 			// aapt resource value: 0x7f050002
-			public const int linearLayout1 = 2131034114;
+			public const int Password = 2131034114;
 			
-			// aapt resource value: 0x7f050000
-			public const int linearLayout2 = 2131034112;
+			// aapt resource value: 0x7f05000b
+			public const int browsebtn = 2131034123;
 			
 			// aapt resource value: 0x7f050008
-			public const int text1 = 2131034120;
+			public const int linearLayout1 = 2131034120;
+			
+			// aapt resource value: 0x7f050006
+			public const int linearLayout2 = 2131034118;
+			
+			// aapt resource value: 0x7f05000a
+			public const int myProfilebtn = 2131034122;
 			
 			// aapt resource value: 0x7f050009
-			public const int text2 = 2131034121;
+			public const int myProjectsbtn = 2131034121;
+			
+			// aapt resource value: 0x7f050003
+			public const int text1 = 2131034115;
+			
+			// aapt resource value: 0x7f050004
+			public const int text2 = 2131034116;
+			
+			// aapt resource value: 0x7f050000
+			public const int textView1 = 2131034112;
 			
 			static Id()
 			{
@@ -107,10 +110,22 @@ namespace WesterdalsApp
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int layout1 = 2130903040;
+			public const int browse = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int Main = 2130903041;
+			public const int logIn = 2130903041;
+			
+			// aapt resource value: 0x7f030002
+			public const int messages = 2130903042;
+			
+			// aapt resource value: 0x7f030003
+			public const int myProfile = 2130903043;
+			
+			// aapt resource value: 0x7f030004
+			public const int myProjects = 2130903044;
+			
+			// aapt resource value: 0x7f030005
+			public const int startingPage = 2130903045;
 			
 			static Layout()
 			{
--- App/WesterdalsApp/obj/Debug/android/src/westerdalsapp/westerdalsapp/R.java
+++ App/WesterdalsApp/obj/Debug/android/src/westerdalsapp/westerdalsapp/R.java
@@ -14,21 +14,26 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int Email=0x7f050006;
-        public static final int Login=0x7f05000a;
-        public static final int Password=0x7f050007;
-        public static final int button1=0x7f050001;
-        public static final int button2=0x7f050003;
-        public static final int button3=0x7f050005;
-        public static final int button4=0x7f050004;
-        public static final int linearLayout1=0x7f050002;
-        public static final int linearLayout2=0x7f050000;
-        public static final int text1=0x7f050008;
-        public static final int text2=0x7f050009;
+        public static final int Email=0x7f050001;
+        public static final int Login=0x7f050005;
+        public static final int MessageButton=0x7f050007;
+        public static final int Password=0x7f050002;
+        public static final int browsebtn=0x7f05000b;
+        public static final int linearLayout1=0x7f050008;
+        public static final int linearLayout2=0x7f050006;
+        public static final int myProfilebtn=0x7f05000a;
+        public static final int myProjectsbtn=0x7f050009;
+        public static final int text1=0x7f050003;
+        public static final int text2=0x7f050004;
+        public static final int textView1=0x7f050000;
     }
     public static final class layout {
-        public static final int layout1=0x7f030000;
-        public static final int main=0x7f030001;
+        public static final int browse=0x7f030000;
+        public static final int login=0x7f030001;
+        public static final int messages=0x7f030002;
+        public static final int myprofile=0x7f030003;
+        public static final int myprojects=0x7f030004;
+        public static final int startingpage=0x7f030005;
     }
     public static final class string {
         public static final int ApplicationName=0x7f040001;
