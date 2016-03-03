d28e60742d4989161be8d1e180ed5e1d1f5e36f2
Kevin Aslan <kevin_aslan@hotmail.com>
2016-03-03 20:51:54 +0100

    Navigering mobilapp forsøk 4

---
+++ php/includes/searchStudResalt.inc.php
@@ -0,0 +1,38 @@
+
+
+<?php
+sec_session_start();
+
+$error_msg = "";
+
+$searchOnUser = "";
+
+if (isset($_POST['searchOnUser'])) { 
+  $searchOnUser = filter_input(INPUT_POST, 'searchOnUser', FILTER_SANITIZE_STRING);
+  
+if (empty($error_msg)&&!($searchOnUser == "")) {
+       if($result = $mysqli->query("SELECT Username,Email,PictureName, AboutUser, CV FROM user left join userprofile on user.ID = userprofile.UserID")){
+        if($count = $result->num_rows){
+            while ($row = $result->fetch_object()) {
+                
+                if($searchOnUser == $row->Username){
+                   $_SESSION["profileUsername"] = $row->Username;
+                   $_SESSION["profileEmail"] = $row->Email;
+                   $_SESSION["profileAboutUser"] = $row->AboutUser;
+                  $_SESSION["profileOnUser"] = $row->PictureName;
+                   
+
+                   $result->free();
+                   header('Location: ./searchStudResalt.php');
+               }
+            }
+        }
+        
+    }
+        // Insert the new user into the database 
+            //Variabel feil, sjekker username opp mot lokal username før den sender inn dataen
+     
+    }
+    echo "User do not exist";
+
+}?>
