5b7651174b7857e72f8758af22f1f82ff632ec37
Marthe Nikoline Elsrud <elsmar14@student.westerdals.no>
2016-04-20 08:50:50 +0200

    Header

--- php/header.php
+++ php/header.php
@@ -60,7 +60,7 @@ if (login_check($mysqli) == true) {
       <div id="headerMenu" class="nav">
       <ul>
         <li><a id="title" class="titleHead" href="index.php"><h2>TUNG?</h2></a></li>
-        <li id="seProsjekter" class="li-border navHoverEffect"><a href="allProjects.php">Se Prosjekter</a></li>
+        <li id="seProsjekter" class="li-border navHoverEffect"><a href="allProjects.php">Se prosjekter</a></li>
         <li id="addJob" class="li-border navHoverEffect"><a href="addproject_page.php"> Legg til et prosjekt </a></li>
         <li id="profileLink" class="li-border navHoverEffect"><?php if (login_check($mysqli) == true) { echo '<a class="" href="userinfo_page.php">Min Profil</a>';
             
