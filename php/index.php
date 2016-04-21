5758603690c793d306bec3548e5813044b827910
Marthe Nikoline Elsrud <elsmar14@student.westerdals.no>
2016-04-21 14:21:33 +0200

    Småting

--- php/index.php
+++ php/index.php
@@ -5,7 +5,7 @@
 
 <div id="aboutSiteDiv" class="">
     
-    <p id="aboutSiteP">TUNG er en studentportal hvor du som Westerdøling kan laste opp prosjekter du har jobbet med som student. Ved å være registrert kan du med din egne profil se på andre sine prosjekter, 'like' andres prosjekter og legge til prosjekter selv. <b>Registrer deg som bruker idag!</b></p>
+    <p id="aboutSiteP">TUNG er en studentportal hvor du som Westerdøling kan laste opp prosjekter du har jobbet med som student. Ved å være registrert kan du med din egne profil se på andre sine prosjekter, 'like' andres prosjekter og legge til prosjekter selv. <b><a href="register.php">Registrer deg som bruker idag!</a></b></p>
 
 </div>
 <!-- Entry of body content field for index below -->
@@ -21,7 +21,7 @@
         <!-- Knapp funkjsonen-->
     <?php else : ?>
     <p>
-        <span class="error">You are not authorized to access this page.</span> Please <a href="login.php" class="linkerStyle">login</a>.
+        <!--<span class="error">You are not authorized to access this page.</span> Please <a href="login.php" class="linkerStyle">login</a>.-->
     </p>
     <?php endif; ?>
     </body>
